53eb3eb7beb270700494dcae88bd1f198c840c60
luanpoppe <luanpoppe@gmail.com>
2025-02-08 20:15:37 -0300

    feat: ajustando o title e favicon do projeto

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,13 @@
 import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling app",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
